5ba3e385b2446cc34ab110f3b9e27427f86760ed
Benedikt Kleiner <benedikt.kleiner@gmail.com>
2016-08-18 18:21:46 +0200

    update server url

--- app/src/main/java/com/ls/utils/ApplicationConfig.java
+++ app/src/main/java/com/ls/utils/ApplicationConfig.java
@@ -1,7 +1,7 @@
 package com.ls.utils;
 
 public class ApplicationConfig {
-    public final static String BASE_URL = "http://drupalconbarcelona2015.uat.link/api/v2/";
+    public final static String BASE_URL = "http://blue00.str8labs.net:8989/api/v2/";
     public static final String PROPERTY_ID = "Goodle Analytisc App ID stub";
     public static final int CACHE_DISK_USAGE_BYTES = 20 * 1024 * 1024;
 }
